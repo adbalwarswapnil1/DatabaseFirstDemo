01302cddafd00481b6b3caffb8658e74d28f51fc
swapnil_adbalwar <swapnil_adbalwar@PSL-HP427L3>
2023-02-14 15:44:16 +0530

    Add project files.

---
+++ DatabaseFirstDemo/Controllers/MoviesController.cs
@@ -0,0 +1,96 @@
+﻿using DatabaseFirstDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseFirstDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly Demo123Context _dbContext;
+
+        public MoviesController(Demo123Context dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            if (_dbContext.Movies == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Movies.ToListAsync();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            if (_dbContext.Movies == null)
+            {
+                return NotFound();
+            }
+            var movie = await _dbContext.Movies.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+            _dbContext.Movies.Add(movie);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(movie);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMovie(int id, Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Entry(movie).State = EntityState.Modified;
+
+
+            await _dbContext.SaveChangesAsync();
+
+
+            return Ok(movie);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            if (_dbContext.Movies == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _dbContext.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Movies.Remove(movie);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
